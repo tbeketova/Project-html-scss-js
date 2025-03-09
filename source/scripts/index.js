d693c5c85c51c11871121c98c59224b7d1400053
Tatiana B <tbeketova04@gmail.com>
2025-03-09 21:24:30 +0200

    added adaptive style for header, features, map

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+const navList = document.querySelector('.navigation');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+  navList.classList.toggle('navigation--closed');
+  navList.classList.toggle('navigation--opened');
+});
