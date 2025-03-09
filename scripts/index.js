e28638ae2dd92278c86b1d858a096aa3a0850dcd
tbeketova <115452305+tbeketova@users.noreply.github.com>
2025-03-09 19:25:47 +0000

    Deploying to gh-pages from @ tbeketova/Project-html-scss-js@d693c5c85c51c11871121c98c59224b7d1400053 🚀

---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".navigation"),t=document.querySelector(".toggle");t.addEventListener("click",()=>{e.classList.toggle("navigation--closed"),e.classList.toggle("navigation--opened")});
